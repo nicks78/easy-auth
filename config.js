87d3d94d6261b3a0f1e6060a3f8aebf87899af78
Nicolas <contact@apx-dev.com>
2020-06-15 01:52:06 +0200

    remove server-copy

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
     SECRET: "mysupersecret",
-    DB_HOSTNAME: "",
+    DB_HOSTNAME: "192.168.0.106",
     DB_PORT: 5432,
     DB_NAME: "easy_auth",
-    DB_USERNAME: "",
-    DB_PASSORD: "",
+    DB_USERNAME: "kiwi",
+    DB_PASSORD: "0000",
 
     GOOGLE: {
-        clientID: "",
-        clientSecret: '',
+        clientID: "3543863861-2d1c51jtu4i17lnhesrmbbme9ledbeoi.apps.googleusercontent.com",
+        clientSecret: 'Iugjwe1djZ36CFcUeNQE1ajt',
         callbackURL: "http://localhost:3001/auth/google/callback",
         scope: ['https://www.googleapis.com/auth/plus.login']
     }
