a4e575861478f4d55f0d0cab94ad4dffbf343e61
Nicolas <contact@apx-dev.com>
2020-06-15 02:02:34 +0200

    rm file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3001
 app.use(cors())
 
 var corsOptions = {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
